a54a85eef57dbd2f5eb69414d6ef6c9aeba8e2b8
Mehdi08 <mehdimna96@gmail.com>
2022-12-17 10:02:14 +0330

    Moved api keys to config file

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,2 +1,5 @@
 String get serverUrl => "https://gateway.marvel.com";
-const apiPath = '/v1/public'; //todo
+const apiPath = '/v1/public';
+const publicKey = '5295c550265a24404f9cace64c971bd6';
+const privateKey = 'cb17168db6c04859f0fb530529dc0dcda49255eb';
+const lazyLoadLimit = 20;
--- lib/core/network/app_interceptor.dart
+++ lib/core/network/app_interceptor.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:crypto/crypto.dart';
+import 'package:marvel_characters/core/config.dart';
 
 class AppInterceptor extends Interceptor {
   AppInterceptor();
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    const publicKey = '5295c550265a24404f9cace64c971bd6'; //todo
-    const privateKey = 'cb17168db6c04859f0fb530529dc0dcda49255eb'; //todo
     final ts = DateTime.now().millisecondsSinceEpoch;
     final hash =
         md5.convert(utf8.encode('$ts$privateKey$publicKey')).toString();
