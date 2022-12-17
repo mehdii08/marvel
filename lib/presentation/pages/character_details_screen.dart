dc35d9318561057844b73673cc99773a177d7f43
Mehdi08 <mehdimna96@gmail.com>
2022-12-17 10:18:55 +0330

    Modified characters_screen.dart and character details screen

--- lib/presentation/pages/character_details_screen.dart
+++ lib/presentation/pages/character_details_screen.dart
@@ -1,20 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18next/i18next.dart';
+import 'package:marvel_characters/core/app_colors.dart';
+import 'package:marvel_characters/domain/entities/character.dart';
+import 'package:marvel_characters/presentation/widgets/resources_chip.dart';
+import 'package:marvel_characters/presentation/widgets/urls_chip.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class CharacterDetailsScreen extends StatefulWidget {
+class CharacterDetailsScreen extends StatelessWidget {
   static const route = "/character/:character_id/details";
-  const CharacterDetailsScreen({Key? key}) : super(key: key);
+  static String generateRoute(int characterId) =>
+      '/character/$characterId/details';
+  final Character character;
+  const CharacterDetailsScreen({Key? key, required this.character})
+      : super(key: key);
 
-  @override
-  State<CharacterDetailsScreen> createState() => _CharacterDetailsScreenState();
-}
-
-class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(context.t('page.characters.title')),
+        title: Text(context.t('page.character_details.title')),
+      ),
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            color: AppColors.placeHolderColor,
+            height: MediaQuery.of(context).size.width,
+            width: MediaQuery.of(context).size.width,
+            child: Hero(
+                tag: 'image:${character.id}',
+                child: Image.network(character.thumbnail.url)),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  character.name,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  character.description,
+                  style: const TextStyle(fontSize: 16),
+                ),
+                if (character.urls.isNotEmpty) ...[
+                  const SizedBox(height: 16),
+                  UrlsChip(
+                    urls: character.urls,
+                    onItemPressed: (url) async {
+                      if (await canLaunchUrlString(url)) {
+                        launchUrlString(url);
+                      }
+                    },
+                  ),
+                ],
+                if (character.comics.available > 0) ...[
+                  const SizedBox(height: 16),
+                  ResourcesChip(resources: character.comics),
+                ],
+                if (character.series.available > 0) ...[
+                  const SizedBox(height: 16),
+                  ResourcesChip(resources: character.series),
+                ],
+                if (character.stories.available > 0) ...[
+                  const SizedBox(height: 16),
+                  ResourcesChip(resources: character.stories),
+                ],
+                if (character.events.available > 0) ...[
+                  const SizedBox(height: 16),
+                  ResourcesChip(resources: character.events),
+                ],
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
--- lib/presentation/pages/characters_screen.dart
+++ lib/presentation/pages/characters_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_i18next/i18next.dart';
+import 'package:go_router/go_router.dart';
+import 'package:marvel_characters/core/app_colors.dart';
+import 'package:marvel_characters/core/errors/failures.dart';
 import 'package:marvel_characters/presentation/blocs/characters_list/characters_cubit.dart';
 import 'package:marvel_characters/presentation/blocs/characters_list/characters_state.dart';
+import 'package:marvel_characters/presentation/pages/character_details_screen.dart';
 import 'package:marvel_characters/presentation/widgets/characters_list_item.dart';
 import 'package:marvel_characters/service_locator.dart';
 
@@ -18,10 +22,12 @@ class CharactersScreen extends StatefulWidget {
 class _CharactersScreenState extends State<CharactersScreen> {
   final _scrollController = ScrollController();
   late CharactersCubit _charactersCubit;
+  bool showingErrorDialog = false;
 
   _onListScrolled() {
     if (_scrollController.position.pixels >
-        _scrollController.position.maxScrollExtent - 100) {
+            _scrollController.position.maxScrollExtent - 100 &&
+        !showingErrorDialog) {
       _charactersCubit.fetchData();
     }
   }
@@ -31,6 +37,50 @@ class _CharactersScreenState extends State<CharactersScreen> {
     super.initState();
     _charactersCubit = sl()..fetchData();
     _scrollController.addListener(_onListScrolled);
+    // showErrorDialog(const NetworkFailure());
+  }
+
+  showErrorDialog(Failure failure) {
+    Dialog errorDialog = Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0)), //this right here
+      child: SizedBox(
+        height: 200.0,
+        width: 300.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                failure.messageIsKey
+                    ? context.t(failure.message!)
+                    : failure.message!,
+                style: const TextStyle(color: Colors.red),
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  showingErrorDialog = false;
+                  _charactersCubit.fetchData();
+                },
+                child: Text(
+                  context.t('errors.try_again'),
+                  style:
+                      const TextStyle(color: AppColors.black, fontSize: 18.0),
+                ))
+          ],
+        ),
+      ),
+    );
+    showingErrorDialog = true;
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => errorDialog,
+        barrierDismissible: false);
   }
 
   @override
@@ -39,17 +89,14 @@ class _CharactersScreenState extends State<CharactersScreen> {
       appBar: AppBar(
         title: Text(context.t('page.characters.title')),
       ),
-      backgroundColor: const Color(0xFFe0e0e0),
+      backgroundColor: AppColors.backgroundColor,
       body: BlocProvider<CharactersCubit>(
         create: (_) => _charactersCubit,
         child: BlocConsumer<CharactersCubit, CharactersState>(
           listener: (context, state) {
-            print('----------> state : $state');
             if (state is CharactersLoadFailed) {
-              print(
-                  '----------> failed : ${(state as CharactersLoadFailed).failure.message}');
+              showErrorDialog(state.failure);
             }
-            print('----------> characters : ${state.characters.length}');
           },
           builder: (context, state) {
             if (state is CharactersLoading && state.characters.isEmpty) {
@@ -74,7 +121,14 @@ class _CharactersScreenState extends State<CharactersScreen> {
                       ),
                     );
                   }
-                  return CharacterListItem(character: characters[index]);
+                  return CharacterListItem(
+                    character: characters[index],
+                    onTap: (character) {
+                      context.push(
+                          CharacterDetailsScreen.generateRoute(character.id),
+                          extra: character);
+                    },
+                  );
                 });
           },
         ),
