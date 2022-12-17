e093ca45b89c697511268205cd454bdb4edc23b5
Mehdi08 <mehdimna96@gmail.com>
2022-12-17 10:53:12 +0330

    Added AppColors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const backgroundColor = Color(0xFFe0e0e0);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const placeHolderColor = Color(0xFFE2E2E2);
+  static const red = Color(0xFFFF0000);
+  static const blue = Color(0xFF0088FF);
+}
